49033e5d9bce0f62cae7f6a87bf322f8482eb588
Arjun-Shukla <arjunhp14s@gmail.com>
2026-04-01 16:44:05 +0530

    fixed bug

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,28 +1,39 @@
-const Razorpay = require('razorpay');
+const Razorpay = require("razorpay");
 
 const instance = new Razorpay({
-    key_id : process.env.RAZORPAY_KEY_ID,
-    key_secret : process.env.RAZORPAY_KEY_SECRET
+
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+
 });
 
-exports.createOrder = async function(req,res){
-try{
-    
-    const options = {
-    
-    amount : 119900,
-    currency : "INR",
-    receipt : "order_recptd_1"
-    
+exports.createOrder = async function (req, res) {
+
+  try {
+
+    const amount = req.body.amount;
+
+    if (!amount || amount <= 0) {
+
+      return res.status(400).send("Invalid amount");
+
     }
-    
+
+    const options = {
+
+      amount: amount * 100,
+      currency: "INR",
+      receipt: "order_" + Date.now(),
+
+    };
+
     const order = await instance.orders.create(options);
     res.json(order);
- 
-}
-catch(err){
-    console.log(err)
-    res.status(501).send("Order Create Not Available");
-}
 
-}
+  } catch (err) {
+
+    console.log(err);
+    res.status(500).send("Order Create Not Available");
+
+  }
+};
