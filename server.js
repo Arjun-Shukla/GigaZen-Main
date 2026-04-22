b59b6e92673332b5be4be171520fcb96e4fac429
Arjun-Shukla <arjunhp14s@gmail.com>
2026-04-22 19:00:16 +0530

    ..

--- server.js
+++ server.js
@@ -8,3 +8,5 @@ const PORT = process.env.PORT;
 app.listen(PORT ,()=>{
     console.log("Server Running Successfully.......");
 });
+
+//server running above this side, running on some port, as said on .env file , marking above but not legally advised to push on github.
